8858a19cc00f826f1e65549ba347584ab5eff2e3
Mustafa <mustafakhattak360@icloud.com>
2025-10-01 09:28:17 +0100

    start

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,51 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+  return ( 
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Countdown />
     </>
   )
+}
+
+function getTime() {
+  let today = new Date();
+  let hrs = 23 - today.getHours();
+  let mins = 59 - today.getMinutes();
+  let secs = 59 - today.getSeconds();
+
+  return [hrs, mins, secs]
+}
+
+function Countdown() {
+
+let today = new Date();
+const yyyy = today.getFullYear();
+let mm = today.getMonth();
+let dd = today.getDate();
+let p = document.querySelector('p')
+
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+let formattedDate = months[mm] + " " + dd + ", " + yyyy
+  const dates = [
+    Date.parse("May 11, 2026") - Date.parse(formattedDate), 
+  ]
+  const milliseconds = 24 * 60 * 60 * 1000
+  let daystill = Math.floor(dates[0]/milliseconds)
+
+let times = getTime()
+
+setInterval(() => {
+  p.innerHTML = 'Time to econ: {daystill} {times[0]} {times[1]} {times[2]}';
+}, 1000);
+  
+  return (
+    <>
+    <p>Time to econ: {daystill}</p>
+    </>
+  )
+  
 }
 
 export default App
